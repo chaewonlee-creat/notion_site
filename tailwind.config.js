a0ab8b5da94ec311d0b24830eef6224f75e7ebc9
Chaewon Lee <haho252@gmail.com>
2025-04-21 10:41:44 +0900

    fix: Tailwind postcss plugin & build cache reset

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: [
     './pages/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
-    './app/**/*.{js,jsx,ts,tsx}'
+    './app/**/*.{js,jsx,ts,tsx}',
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
